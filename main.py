6c533b25bb68e6424b73db1bb3ed412e1cb0eb5c
sujal koli <sujalkoli03@gmail.com>
2024-07-06 20:31:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, send_file, render_template
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+
+nltk.download('vader_lexicon')
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        inp = request.form.get("inp")
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(inp)
+        if score["neg"] != 0:
+            return render_template("index.html", message="Negative Feedback ☹️☹️")
+        else:
+            return render_template("index.html", message="Positive Feed back 😁😁")
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
